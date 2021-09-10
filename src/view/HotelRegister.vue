6124cfb68ebaf6a432be1b69deeeac3506e9a85c
zb2313 <2224105295@qq.com>
2021-09-10 22:53:27 +0800

    Update HotelRegister.vue

--- src/view/HotelRegister.vue
+++ src/view/HotelRegister.vue
@@ -1,71 +1,49 @@
 <template>
-  <div
-    class="bgc"
-    :style="{
-      backgroundImage: 'url(' + coverImgUrl + ')',
-      backgroundSize: '100% 100%',
-      backgroundRepeat: 'no-repeat',
-    }"
-  >
-    <div class="main">
-      <div class="dashboard-container">
-        <div class="title"><h1>商家入驻</h1></div>
-        <br />
-        <el-form ref="form" :model="form" label-width="100px">
-          <el-form-item label="商家名称">
-            <el-input style="width: 380px" v-model="form.hotel_name" />
-          </el-form-item>
-          <el-form-item label="密码">
-            <el-input style="width: 380px" v-model="form.Password" />
-          </el-form-item>
-          <el-form-item label="确认密码">
-            <el-input
-              type="password"
-              style="width: 380px"
-              v-model="form.vPassword"
-            />
-          </el-form-item>
-          <el-form-item label="身份证号">
-            <el-input style="width: 380px" v-model="form.id_number" />
-          </el-form-item>
-          <el-form-item label="手机">
-            <el-input style="width: 380px" v-model="form.tele_NUMBER" />
-          </el-form-item>
-          <el-form-item label="E-mail">
-            <el-input style="width: 268px" v-model="form.mail" />
-            <el-button type="primary" @click="sendVerifyCode"
-              >发送验证码</el-button
-            >
-          </el-form-item>
-          <el-form-item label="邮箱验证码" v-if="verifyStatus">
-            <el-input style="width: 250px" v-model="form.verifycode"></el-input>
-          </el-form-item>
-          <el-form-item label="地点">
-            <el-input style="width: 380px" v-model="form.location"> </el-input>
-          </el-form-item>
+  <div class="bgc">
+    <div class="dashboard-container">
+      <div class="title"><h1>商家入驻</h1></div>
+      <br />
+      <el-form ref="form" :model="form" label-width="100px">
+        <el-form-item label="商家ID">
+          <el-input style="width: 380px" v-model="form.hotel_id" />
+        </el-form-item>
+        <el-form-item label="商家名称">
+          <el-input style="width: 380px" v-model="form.hotel_name" />
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input style="width: 380px" v-model="form.Password" />
+        </el-form-item>
+        <el-form-item label="确认密码">
+          <el-input type="password" style="width: 380px" v-model="form.vPassword" />
+        </el-form-item>
+        <el-form-item label="身份证号">
+          <el-input style="width: 380px" v-model="form.id_number" />
+        </el-form-item>
+        <el-form-item label="手机">
+          <el-input style="width: 380px" v-model="form.tele_NUMBER" />
+        </el-form-item>
+        <el-form-item label="地点">
+          <el-input style="width: 380px" v-model="form.location"> </el-input>
+        </el-form-item>
 
-          <el-upload
+        <el-upload
             class="upload-demo"
             :action="getUploadUrl"
             :on-preview="handlePreview"
             :on-remove="handleRemove"
             :file-list="fileList"
-            list-type="string"
-          >
-            <el-button size="small" type="primary" style="margin-left: 213px"
-              >点击上传资质证明</el-button
-            >
-            <div slot="tip" class="el-upload__tip" style="margin-left: 170px">
-              只能上传jpg/png文件，且不超过500kb
-            </div>
-          </el-upload>
+            list-type="string">
+          <el-button size="small" type="primary">点击上传资质证明</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
 
-          <div style="text-align: center; margin-top: 20px">
-            <el-button type="primary" @click="onSubmit">注册</el-button>
-            <el-button @click="goBack">取消</el-button>
-          </div>
-        </el-form>
-      </div>
+
+        <div style="text-align: center; margin-top: 40px">
+          <el-button type="primary" @click="onSubmit">注册</el-button>
+          <el-button @click="goBack">取消</el-button>
+
+        </div>
+      </el-form>
     </div>
   </div>
 </template>
@@ -81,35 +59,36 @@ export default {
   data() {
     return {
       form: {
-        mail: "",
-        hotel_name: "", //用户名
-        Password: "", //密码
-        vPassword: "",
-        user_ID: "", //用户编号
-        id_number: "",
-        Gender: "", //性别
+        mail:"",
+        hotel_name: "",//用户名
+        hotel_id:"",
+        Password: "",//密码
+        vPassword:"",
+        user_ID: "",//用户编号
+        id_number:"",
+        Gender:"",//性别
         desc: "",
-        tele_NUMBER: "", //电话号码
-        mailbox_ID: "", //
+        tele_NUMBER: "",//电话号码
+        mailbox_ID: "",//
         dState: true,
-        verifycode: "",
-        location: " ",
+        verifycode:"",
+        location:" "
       },
-      coverImgUrl: require("../assets/img/login-bg.jpg"),
       testInfo: "",
       //省市区
       province: "aaaaa",
       city: "",
       area: "",
-      location: " ",
+      location:" ",
       show: false,
-      verifyStatus: false,
+      verifyStatus:false,
       fileList: [],
     };
   },
   methods: {
-    getUploadUrl() {
-      return "http://49.234.18.247:8080/api/HotelLicense/" + this.setUserID();
+    getUploadUrl()
+    {
+      return "http://49.234.18.247:8080/api/HotelLicense/"+this.setUserID();
     },
     handleRemove(file, fileList) {
       console.log(file, fileList);
@@ -117,57 +96,70 @@ export default {
     handlePreview(file) {
       console.log(file);
     },
-    returnToLogin() {
+    returnToLogin()
+    {
       this.$router.push("/Login");
     },
-    setUserID() {
-      let chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-      let ID = "";
-      for (let i = 0; i < 10; i++) {
-        let id = Math.ceil(Math.random() * 9);
-        ID += chars[id];
+    setUserID()
+    {
+      let chars = ['0','1','2','3','4','5','6','7','8','9'];
+      let ID='';
+      for(let i=0;i<10;i++)
+      {
+        let id = Math.ceil(Math.random()*9);
+        ID+=chars[id];
       }
-      this.form.user_ID = ID;
+      this.form.user_ID=ID;
       return ID;
     },
-    setMailBoxID() {
-      this.form.mailbox_ID = this.form.user_ID;
+    setMailBoxID()
+    {
+      this.form.mailbox_ID=this.form.user_ID;
     },
-    goBack() {
+    goBack()
+    {
       this.$router.push("/Login");
     },
     onSubmit() {
-      axios.post("http://49.234.18.247:8080/api/Hotel", {
-        hoteL_ID: "1000000000",
-        hoteL_NAME: this.form.hotel_name,
-        hlocation: this.form.location,
-        pictrue: "1",
-        star: 1,
-        lowesT_PRICE: 1,
-        hpassword: this.form.Password,
-        label: "lalala",
+      axios.post("http://49.234.18.247:8080/api/Hotel",
+      {
+        "hoteL_ID":this.form.hotel_id,
+        "hoteL_NAME":this.form.hotel_name,
+        "hlocation":this.form.location,
+        "pictrue":"1",
+        "star":1,
+        "lowesT_PRICE":1,
+        "hpassword":this.form.Password,
+        "label":"lalala",
+        "opeN_TIME": "string",
+        "totaL_NUM": 0,
+        "telephone": this.form.tele_NUMBER,
+        "description": "string",
+        "iS_CHECK": 0
       });
+      this.$message.success("申请成功，请等待管理员审核");
     },
-    onCancel() {
+    onCancel(){
       this.$message({
         message: "cancel!",
         type: "warning",
       }),
-        this.$router.push("/Login");
+          this.$router.push("/Login");
     },
     //打开选择地区
-    sendVerifyCode() {
-      axios.get(
-        "http://49.234.18.247:8080/api/Email/" + this.form.mail + "&register"
-      );
-      this.verifyStatus = true;
-    },
+    sendVerifyCode()
+    {
+      axios.get("http://49.234.18.247:8080/api/Email/"+this.form.mail+"&register");
+      this.verifyStatus=true;
+    }
   },
   created() {
     this.setUserID();
     this.setMailBoxID();
   },
-  mounted: function () {},
+  mounted: function () {
+
+  },
 };
 </script>
 
@@ -176,33 +168,29 @@ h1 {
   margin: 0;
   padding: 0;
 }
-.main {
-  height: 100%;
-  width: 650px;
-  display: flex;
-  align-items: center;
-}
 .bgc {
+  background: url("../assets/img/login-bg.jpg") no-repeat;
+  background-position: center;
   height: 100%;
   width: 100%;
+  background-size: cover;
+  position: fixed;
 }
 .dashboard-container {
-  padding-top: 20px;
   width: 550px;
   height: 650px;
-  margin: 0px auto;
-  border-radius: 5px;
-  background-color: rgba(255, 255, 255, 0.5);
+  margin:70px auto;
+  background-color: rgba(99, 126, 147, 0.58);
 }
 .title {
   text-align: center;
-  color: #2d8cf0;
+  color: white;
 }
 .el-form /deep/ .el-form-item__label {
-  color: #2d8cf0;
+  color: white;
 }
 .el-form /deep/ .el-radio__label {
-  color: #2d8cf0;
+  color: white;
 }
 /*省市区三级联动*/
 .divwrap {
